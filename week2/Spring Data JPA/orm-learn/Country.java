588249a2b8858a866712b102f7512225bb457bcf
Sai Nihar Kunala <k.sainihar2006@gmail.com>
2026-07-09 00:07:21 +0530

    fix spring boot package scan and database load order

--- week2/Spring Data JPA/orm-learn/Country.java
+++ week2/Spring Data JPA/orm-learn/Country.java
@@ -1,3 +1,5 @@
+package ormlearn;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
--- week2/Spring Data JPA/orm-learn/CountryService.java
+++ week2/Spring Data JPA/orm-learn/CountryService.java
@@ -1,3 +1,5 @@
+package ormlearn;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
